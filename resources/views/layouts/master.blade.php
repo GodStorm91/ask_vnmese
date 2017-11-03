6801beeddb840878a6a000f101f4c7b76f0ecc60
Khanh Nguyen <godstorm91@gmail.com>
2017-11-04 01:21:08 +0900

    Init commit

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css">
+    <title>My App</title>
+</head>
+<body>
+    @include ('layouts.header')
+    <div id="app">
+    <section class="section">
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </section>
+</div>
+<script type="text/javascript" src="/js/app.js"></script>
+</body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,12 @@
+    <nav class="tabs">
+      <div class="container">
+        <ul>
+          <router-link tag="li" to="/" exact>
+            <a href="#/">Home</a>
+          </router-link>
+          <router-link tag='li' to="/about">
+            <a <a href="#/about">About</a>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
